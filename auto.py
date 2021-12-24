3599338b0da704583d6387f323c40f4eeec271b9
keshav <keshavjha1504@gmail.com>
2021-12-25 03:16:08 +0530

    Update auto.py

--- auto.py
+++ auto.py
@@ -4,13 +4,13 @@ import time
 print("Started")
 for k in range(30):
     webbrowser.open("https://keshavbits.herokuapp.com")
-    time.sleep(9)
+    time.sleep(7)
     # pyautogui.moveTo(900, 500)
-    time.sleep(4)
+    # time.sleep(4)
     # pyautogui.click()
-    time.sleep(8)
+    # time.sleep(8)
     
-    time.sleep(2)
+    # time.sleep(2)
     print(30-k)
     
 
