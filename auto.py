70fb640e1e79c4105c772c7ee212d693b9fff8c7
keshav <keshavjha1504@gmail.com>
2021-12-24 21:25:23 +0530

    Update auto.py

--- auto.py
+++ auto.py
@@ -2,15 +2,16 @@ import webbrowser
 import time
 import pyautogui
 print("Started")
-for k in range(10):
+for k in range(40):
     webbrowser.open("https://keshavbits.herokuapp.com")
-    time.sleep(6)
+    time.sleep(8)
     pyautogui.moveTo(900, 500)
-    time.sleep(2)
-    pyautogui.click()
     time.sleep(4)
+    pyautogui.click()
+    time.sleep(6)
     pyautogui.hotkey('ctrl', 'w')
-    time.sleep(2) 
+    time.sleep(2)
+    pyautogui.hotkey('ctrl', 'w') 
 
 
 
