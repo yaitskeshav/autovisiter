67110020204a8a67e3ea17b2080a496601ad4890
keshav <keshavjha1504@gmail.com>
2021-12-25 20:38:49 +0530

    Update auto.py

--- auto.py
+++ auto.py
@@ -3,15 +3,15 @@ import time
 import pyautogui
 print("Started")
 n=1
-while n<1000:
+while n<100:
     webbrowser.open("https://keshavbits.herokuapp.com")
     time.sleep(7)
     pyautogui.moveTo(900, 500)
     time.sleep(4)
     pyautogui.click()
-    time.sleep(8)
+    time.sleep(5)
     
-    time.sleep(2)
+    # time.sleep(2)
     n=n+1
     print(n)
     
