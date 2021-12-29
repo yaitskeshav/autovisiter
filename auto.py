94c8be039496ea3b3324fe3e38446fbc4d8b4c74
keshav <keshavjha1504@gmail.com>
2021-12-29 20:40:58 +0530

    Update auto.py

--- auto.py
+++ auto.py
@@ -3,19 +3,22 @@ import time
 import pyautogui
 print("Started")
 n=1
-while n<100:
+while n<200:
     webbrowser.open("https://keshavbits.herokuapp.com")
     time.sleep(7)
     pyautogui.moveTo(900, 500)
-    time.sleep(4)
+    time.sleep(2)
     pyautogui.click()
     time.sleep(5)
-    
-    # time.sleep(2)
+    if n%3==0:
+       pyautogui.hotkey("alt","w")
+       time.sleep(2)
+       print("closed")
+    if(n%30==0):
+	time.sleep(5)
+   
     n=n+1
     print(n)
     
-
-
 
 print("ended")
